abb4720f11de9c722efffc99faa9f936cd11a9a2
Alexey Rezakov <a.rezakov@rambler-co.ru>
2016-05-10 14:13:27 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,122 @@
+'use strict';
+
+var Promise = require('lie');
+
+var HOST_NAME = location.hostname;
+var PLATE_COOKIE = 'adblock_plate_closed';
+var PLATE_EXPIRES = 3600 * 24;
+
+var SETTINGS_DEV = {
+  cookie: 'dev_c_adbl_sid',
+  createUrl: 'http://noadblock.rambler.ru/createsid',
+  // checkUrl: 'https://api.facebook.com/method/links.getStats?urls=ya.ru&format=json',
+  checkUrl: 'http://noadblock.rambler.ru/checksid',
+  verifyUrl: 'http://noadblock.rambler.ru/verify?content=' + HOST_NAME,
+};
+
+var SETTINGS_PROD = {
+  cookie: 'c_adbl_sid',
+  createUrl: 'http://adblock.rambler.ru/createsid',
+  checkUrl: 'http://adblock.rambler.ru/checksid',
+  verifyUrl: 'http://adblock.rambler.ru/verify?content=' + HOST_NAME,
+};
+
+/**
+ * return show or hide ad and plate
+ */
+function init(isProd) {
+  var settings = (isProd === 'true') ? SETTINGS_PROD : SETTINGS_DEV;
+  var adblockCookie = getCookie(settings.cookie);
+  var result = {};
+  result.plate = !getCookie(PLATE_COOKIE);
+
+  // ====================================
+  // setCookie(settings.cookie, '322323323');
+  // ====================================
+
+  return new Promise(function(resolve, reject) {
+    var request = new XMLHttpRequest();
+    if (!adblockCookie) {
+      result.ad = true;
+      resolve(result);
+      return;
+    }
+    request.open('GET', settings.checkUrl + '?SID=' + adblockCookie, true);
+    request.onload = function() {
+      console.log(request.status);
+      if (request.status === 200) {
+        result.ad = false;
+        result.plate = false;
+        resolve(result);
+        return;
+      }
+      if (request.status === 404) {
+        // deleteCookie(settings.cookie, '.rambler.ru');
+        deleteCookie(settings.cookie);
+        console.log('Удаляем куку');
+      }
+      result.ad = true;
+      resolve(result);
+    };
+    request.onerror = function() {
+      result.ad = true;
+      reject(result);
+    };
+    request.send();
+  });
+}
+
+/**
+ * set plate cookie
+ */
+function delaySubscribe() {
+  setCookie(PLATE_COOKIE, '1', {
+    expires: PLATE_EXPIRES,
+    path: '/'
+  });
+}
+
+function getCookie(name) {
+  var matches = document.cookie.match(new RegExp(
+    '(?:^|; )' + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + '=([^;]*)'
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function setCookie(name, value, options) {
+  var _options = options || {};
+  var d = new Date();
+  var _value = encodeURIComponent(value);
+  var expires = _options.expires;
+  var updatedCookie = name + '=' + _value;
+
+  if (typeof expires === 'number' && expires) {
+    d.setTime(d.getTime() + expires * 1000);
+    expires = _options.expires = d;
+  }
+  if (expires && expires.toUTCString) {
+    _options.expires = expires.toUTCString();
+  }
+  for (var propName in _options) {
+    updatedCookie += '; ' + propName;
+    var propValue = _options[propName];
+    if (propValue !== true) {
+      updatedCookie += '=' + propValue;
+    }
+  }
+  document.cookie = updatedCookie;
+}
+
+function deleteCookie(_name, _domain) {
+  var domain = _domain || '';
+
+  setCookie(_name, '', {
+    domain: domain,
+    expires: -1
+  });
+}
+
+module.exports = {
+  init: init,
+  delaySubscribe: delaySubscribe
+}
